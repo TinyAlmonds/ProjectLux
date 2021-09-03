4dec47096b4b77a7cfd40cf7621472254bd21d11
alexnoerdemann <alex.noerdemann@gmx.de>
2021-09-03 17:43:10 +0200

    feat: Added default UE 4 project to start development

---
+++ Source/ProjectLux/ProjectLuxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectLuxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLUX_API AProjectLuxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
