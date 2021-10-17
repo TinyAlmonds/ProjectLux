47a8fc38a2cca460abb10a1367b3696f0fa475a0
alexnoerdemann <alex.noerdemann@gmx.de>
2021-10-17 13:17:29 +0200

    feat(source-core): Added new PlayerController for refactoring from Blueprint to C++

---
+++ Source/ProjectLux/Public/Core/ProjectLuxPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ProjectLuxPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLUX_API AProjectLuxPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
