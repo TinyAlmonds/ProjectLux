2bef7240d84695b099da30c4f170f600047870d6
alexnoerdemann <alex.noerdemann@gmx.de>
2021-10-17 14:07:11 +0200

    feat(source-core): Bound PlayerController to Input

--- Source/ProjectLux/Private/Core/ProjectLuxPlayerController.cpp
+++ Source/ProjectLux/Private/Core/ProjectLuxPlayerController.cpp
@@ -3,3 +3,34 @@
 
 #include "Core/ProjectLuxPlayerController.h"
 
+AProjectLuxPlayerController::AProjectLuxPlayerController()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AProjectLuxPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    // Bind to input action/axis mappings which redirects to the controlled pawn
+    InputComponent->BindAction("Jump", IE_Pressed, this, &AProjectLuxPlayerController::JumpPressed);
+    InputComponent->BindAction("Jump", IE_Released, this, &AProjectLuxPlayerController::JumpReleased);
+    InputComponent->BindAxis("MoveUp", this, &AProjectLuxPlayerController::MoveUp);
+    InputComponent->BindAxis("MoveRight", this, &AProjectLuxPlayerController::MoveRight);
+}
+
+void AProjectLuxPlayerController::JumpPressed()
+{
+}
+
+void AProjectLuxPlayerController::JumpReleased()
+{
+}
+
+void AProjectLuxPlayerController::MoveRight(float AxisValue)
+{
+}
+
+void AProjectLuxPlayerController::MoveUp(float AxisValue)
+{
+}
