e53044104fd93cf3ff431d9d2fd3b5ef6371265b
alexnoerdemann <alex.noerdemann@gmx.de>
2021-10-20 13:26:49 +0200

    docs(source-core): Updated parameters and ranges of the methods
    -class: ProjectLuxPlayerController

--- Source/ProjectLux/Public/Core/ProjectLuxPlayerController.h
+++ Source/ProjectLux/Public/Core/ProjectLuxPlayerController.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -27,9 +25,15 @@ private:
 	/** Method bound to the "Jump" input action mapping, when the button is released. Redirects the input to the related method of the ProjectLuxCharacter. */
 	void JumpRelease();
 
-	/** Method bound to the "MoveRight" input axis mapping. Redirects the input to the related method of the ProjectLuxCharacter. */
+	/**
+	 * Method bound to the "MoveRight" input axis mapping. Redirects the input to the related method of the ProjectLuxCharacter.
+	 * @param AxisValue - The current axis value (range: -1.0 to 1.0).
+	 */
 	void MoveRight(float AxisValue);
 
-	/** Method bound to the "MoveUp" input axis mapping. Redirects the input to the related method of the ProjectLuxCharacter. */
+	/**
+	 * Method bound to the "MoveUp" input axis mapping. Redirects the input to the related method of the ProjectLuxCharacter.
+	 * @param AxisValue - The current axis value (range: -1.0 to 1.0).
+	 */
 	void MoveUp(float AxisValue);	
 };
