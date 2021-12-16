3ec8b4fb5eb2f682ccfea93d9dbd9fe6b9be65bb
alexnoerdemann <alex.noerdemann@gmx.de>
2021-12-16 15:18:37 +0100

    feat(source-core): Created AttributeSet for the LuxCharacter
    -class: UProjectLuxCharacterAttributeSet
    
    - Just created from the Editor without any functionality yet

---
+++ Source/ProjectLux/Public/ProjectLuxCharacterAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AttributeSet.h"
+#include "CoreMinimal.h"
+#include "ProjectLuxCharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLUX_API UProjectLuxCharacterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
