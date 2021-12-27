5980d47dbe82990f9d1ea44d028d5426021dbe28
alexnoerdemann <alex.noerdemann@gmx.de>
2021-12-27 15:34:50 +0100

    feat(source-core): Added Attributes for combat to AttributeSet
    -class: UProjectLuxCharacterAttributeSet

--- Source/ProjectLux/Private/ProjectLuxCharacterAttributeSet.cpp
+++ Source/ProjectLux/Private/ProjectLuxCharacterAttributeSet.cpp
@@ -3,3 +3,28 @@
 
 #include "ProjectLuxCharacterAttributeSet.h"
 
+UProjectLuxCharacterAttributeSet::UProjectLuxCharacterAttributeSet() : 
+	Health{1.0f},
+	MaxHealth{1.0f},
+	RawDamage{1.0f},
+	Armor{1.0f},
+	MinEmotionalDamageMultiplier{0.0f},
+	MinEmotionalResistance{0.0f},
+	MaxEmotionalResistance{1.0f},
+	FearDamageMultiplier{0.0f},
+	FearResistance{1.0f},
+	AngerDamageMultiplier{0.0f},
+	AngerResistance{1.0f},
+	JoyDamageMultiplier{0.0f},
+	JoyResistance{1.0f},
+	SadnessDamageMultiplier{0.0f},
+	SadnessResistance{1.0f},
+	TrustDamageMultiplier{0.0f},
+	TrustResistance{1.0f},
+	LoathingDamageMultiplier{0.0f},
+	LoathingResistance{1.0f},
+	AnticipationDamageMultiplier{0.0f},
+	AnticipationResistance{1.0f},
+	SupriseDamageMultiplier{0.0f},
+	SupriseResistance{1.0f}
+{}
